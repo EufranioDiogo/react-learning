4a9d78980ee62c5993b430d42342e905c266e891
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-10-09 07:10:57 +0100

    chore: lesson 20

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,19 +2,20 @@ import React from 'react'
 import Person from './Person';
 
 export default function NameList() {
+  const names = ['Bruce', 'Clark', 'Diana']
   const people = [
     { id: 1, name: 'Eufránio', age: 30, skill: 'React' }, 
     { id: 2, name: 'Creuma', age: 25, skill: 'Angular' },
     { id: 3, name: 'Diana', age: 25, skill: 'Vue' }
   ];
 
-  const peopleList = people.map((person) => ( <Person person={person}></Person>
+  const nameList = names.map((name, index) => ( <h2 key={index}>{ name }</h2>
   ))
 
   return (
     <div>
       {
-        peopleList
+        nameList
       }
     </div>
   )
