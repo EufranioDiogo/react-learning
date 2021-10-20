96cd140eb58bb9a0c970a0b43253d4884e6905fd
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-10-21 00:56:39 +0100

    chore: lesson 22 useContext and useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import './App.css';
-import React from 'react';
-import Counter from './Learning Hooks/Lesson 21/Counter';
+import React, { useReducer } from 'react';
+import Counter from './Learning Hooks/Lesson 22/Counter';
+
 
 export const UserContext = React.createContext();
+export const CounterContext = React.createContext();
+
+
+const initialState = {
+  counter1: 0,
+  counter2: 0
+}
+
+const reducer = (currentState, action) => {
+  switch (action.type) {
+    case 'increment1':
+      return {
+        ...currentState,
+        counter1: currentState.counter1 + action.value
+      }
+    case 'decrement1':
+      return {
+        ...currentState,
+        counter1: currentState.counter1 - action.value
+      }
+    case 'reset1':
+      return {
+        ...currentState,
+        counter1: currentState.counter1 + action.value
+      }
+
+    case 'increment2':
+      return {
+        ...currentState,
+        counter2: currentState.counter2 + action.value
+      }
+    case 'decrement2':
+      return {
+        ...currentState,
+        counter2: currentState.counter2 - action.value
+      }
+    case 'reset2':
+      return {
+        ...currentState,
+        counter2: currentState.counter2 + action.value
+      }
+
+    default:
+      break;
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <CounterContext.Provider value={{ initialState, reducer }}>
+        <Counter></Counter>
+        <Counter></Counter>
+      </CounterContext.Provider>
+
 
     </div>
   );
---
+++ src/Learning Hooks/Lesson 22/Counter.js
@@ -0,0 +1,71 @@
+import React, { useReducer, useContext } from 'react'
+import { CounterContext } from '../../App';
+
+
+function Counter() {
+  const counter = useContext(CounterContext);
+  const [counter1, dispatchCounter1] = useReducer(counter.reducer, counter.initialState)
+  const [counter2, dispatchCounter2] = useReducer(counter.reducer, counter.initialState)
+
+
+  return (
+    <div style={{ 
+      borderBottom: '1px solid #333',
+      marginBottom: 100
+    }}>
+
+      <div className="counter-1">
+        <h1>Counter 1: {counter1.counter1}</h1>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter1({ type: 'increment1', value: 1 })
+        }}>Increment</button>
+
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter1({ type: 'increment1', value: 5 })
+        }}>Increment 5</button>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter1({ type: 'decrement1', value: 1 })
+        }}>Decrement</button>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter1({ type: 'decrement1', value: 5 })
+        }}>Decrement 5</button>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter1({ type: 'reset1', value: 1 })
+        }}>Reset</button>
+      </div>
+
+      <div className="counter-2">
+        <h1>Counter 2: {counter2.counter2}</h1>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter2({ type: 'increment2', value: 1 })
+        }}>Increment</button>
+
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter2({ type: 'increment2', value: 5 })
+        }}>Increment 5</button>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter2({ type: 'decrement2', value: 1 })
+        }}>Decrement</button>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter2({ type: 'decrement2', value: 5 })
+        }}>Decrement 5</button>
+        <button onClick={() => {
+          // @ts-ignore
+          dispatchCounter2({ type: 'reset2', value: 1 })
+        }}>Reset</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Counter
